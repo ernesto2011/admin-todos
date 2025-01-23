2f2dab4cb71371df3ec401763c803084abe1993b
ernesto2011 <skillmx107@gmail.com>
2025-01-22 23:36:48 -0600

    total to pay

--- src/app/dashboard/cart/page.tsx
+++ src/app/dashboard/cart/page.tsx
@@ -1,3 +1,4 @@
+import { WidgetItem } from "@/components";
 import { Product, products } from "@/products/data/products";
 import { ItemCard } from "@/shopping-cart";
 import { cookies } from "next/headers";
@@ -27,6 +28,7 @@ export default function CartPage() {
     const cookieStore = cookies()
     const cart = JSON.parse(cookieStore.get('cart')?.value ?? '{}') as {[id:string]:number}
     const productsInCart = getProductsInCart(cart)
+    const totalToPay = productsInCart.reduce((prev, current)=>(current.product.price * current.quantity) + prev,0)
   return (
     <div>
         <h1 className='text-5xl'>Productos en el carrito</h1>
@@ -39,6 +41,14 @@ export default function CartPage() {
                     ))
                 
                 }
+            </div>
+            <div className="flex flex-col w-full sm:w-4/12 ">
+                <WidgetItem title="Total a pagar">
+                   <div className="mt-2 flex justify-center gap-4">
+                        <h3 className="text-3xl font-bold text-gray-600">{(totalToPay * 1.15).toFixed(2)}</h3>
+                   </div>
+                   <span className="font-bold text-center text-gray-500">Impuestos 15%: ${(totalToPay * 0.15).toFixed()}</span>
+                </WidgetItem>
             </div>
         </div>
     </div>
