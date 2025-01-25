eec33b0ca81d22b73c87a9d3c7c5c551f48871c8
ernesto2011 <skillmx107@gmail.com>
2025-01-24 22:32:49 -0600

    ....

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,36 @@
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
 import Google from "next-auth/providers/google"
+import prisma from "./lib/prisma"
+import { Adapter } from "next-auth/adapters"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma) as Adapter,
+  session: { strategy: "jwt" },
   providers: [GitHub, Google ],
+  callbacks: {
+    async signIn({user}){
+      return true
+    },
+    async jwt({token,user, account, profile}){
+      const dbUser = await prisma.user.findUnique({
+        where: {email: token.email  ?? 'no-email'}});
+
+      token.roles = dbUser?.roles ?? [];
+      token.id = dbUser?.id ?? '';
+      return token;
+    },
+    async session({session, token, user}){
+      if ( session && session.user ) {
+        session.user.roles= token.roles as string[];
+        session.user.id = token.id as string
+
+      }
+      console.log({session});
+      
+      return session;
+    }
+
+  }
 })
